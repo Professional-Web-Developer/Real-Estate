0bcc409b37f9139e843accd577b0be0f55789ae7
kavin <pasupathy.kavinmca@gmail.com>
2024-03-12 22:16:09 +0530

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
-
+import {Routes,Route} from  'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <Routes>
+        <Route path='/' element={
+          <>
+        <Navbar/>
+        <Hero/>
+        <PopularProperties/>
+        <FeaturesProperties/>
+        <NewsLetter/>
+
+        <Footer/>
+          </>
+      }/>
+        <Route path='/signup' element={Signup}/>
+        <Route path='/signin' element={SignIn}/>
+        <Route path='/properties' element={
+          <>
+          <Navbar/>
+          <Properties/>
+          <Footer/>
+          </>
+        }/>
+        <Route path='/propertyDetail/:id' element={
+          <>
+          <Navbar/>
+          <PropertyDetail/>
+          <Footer/>
+          </>
+        }/>
+      </Routes>
     </div>
   );
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import classes from './navbar.module.css'
+import {Link} from 'react-router-dom'
+import {BsHouseDoor} from 'react-icons/bs';
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <Link to='/' className={classes.left}>
+          Real Estate <BsHouseDoor/>
+        </Link>
+        <ul className={classes.center}>
+          <li className={classes.listItem}></li>
+          <li className={classes.listItem}></li>
+          <li className={classes.listItem}></li>
+          <li className={classes.listItem}></li>
+        </ul>
+        <div className={classes.right}>
+          <Link to='/signup'>Sign Up</Link>
+          <Link to='/signin'>Sign In</Link>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
