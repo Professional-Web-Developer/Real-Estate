2cf34691e7023b7848f4f8eafcd06f35b9a544a9
kavin <pasupathy.kavinmca@gmail.com>
2024-03-13 11:48:49 +0530

    client

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './hero.module.css'
+import {AiOutlineSearch} from 'react-icons/ai'
 function Hero() {
+  const[type,setType]=useState("Beach")
+  const[Continent,setcontinent]=useState("0")
+  const[priceRange,setpriceRange]=useState("0")
+  const handlesearch=()=>{
+    
+  }
   return (
-    <div>Hero</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2>Let me find your dream place right now</h2>
+        <h5>Search the best selection of luxury real estate</h5>
+        <div className={classes.options}>
+          <select onChange={(e)=>setType(e.target.value)}>
+            <option disabled>Select Type</option>
+            <option value="Beach">Beach</option>
+            <option value="Mountain">Mountain</option>
+            <option value="Village">Village</option>
+          </select>
+          <select onChange={(e)=> setpriceRange(e.target.value)}>
+            <option disabled>Select Price Range</option>
+            <option value="0">0-1,00,000</option>
+            <option value="1">1,00,000-2,00,000</option>
+            <option value="2">2,00,000-3,00,000</option>
+            <option value="3">3,00,000-4,00,000</option>
+            <option value="4">4,00,000-5,00,000</option>
+          </select>
+          <select onChange={(e)=> setcontinent(e.target.value)}>
+            <option disabled>Select Continent</option>
+            <option value="0">Asia</option>
+            <option value="1">Europe</option>
+            <option value="2">Africa</option>
+            <option value="3">South America</option>
+            <option value="4">North America</option>
+            <option value="5">Oceania</option>
+          </select>
+          <AiOutlineSearch className={classes.searchIcon}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/popularProperties/PopularProperties.jsx
+++ client/src/components/popularProperties/PopularProperties.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './popularproperties.module.css'
+import { Link } from "react-router-dom"
+import img1 from "../../assets/realestatebeach.jpg"
+import img2 from "../../assets/realestatemountain.jpg"
+import img3 from "../../assets/realestatecountryside.jpg"
+
 function PopularProperties() {
+  const [numProperties,setNumProperties]=useState({})
+  useEffect(()=>{
+    const fetchNumberProperties= async()=>{
+      try{
+        const data=await request
+      }catch(err){
+        console.log(err.message)
+      }
+    }
+  },[])
   return (
-    <div>PopularProperties</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.titles}>
+          <h5>Different Types Of Properties</h5>
+          <h2>Best Types of Properties For You</h2>
+        </div>
+        <div className={classes.properties}>
+          <Link to={'/properties?type=beach&continent=1&priceRange=2'}>
+            <img src={img1}/>
+            <div className={classes.quantity}>34Properties</div>
+            <h5>Beach Properties</h5>
+          </Link>
+          <Link to={'/properties?type=mountain&continent=1&priceRange=2'}>
+            <img src={img2}/>
+            <div className={classes.quantity}>34Properties</div>
+            <h5>Mountain Properties</h5>
+          </Link>
+          <Link to={'/properties?type=village&continent=1&priceRange=2'}>
+            <img src={img3}/>
+            <div className={classes.quantity}>34Properties</div>
+            <h5>Village Properties</h5>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
