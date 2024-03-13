6b0c7caba7a7e7b5da7194b45db4ececfcdf0619
kavin <pasupathy.kavinmca@gmail.com>
2024-03-13 15:51:37 +0530

    client

--- client/src/components/popularProperties/PopularProperties.jsx
+++ client/src/components/popularProperties/PopularProperties.jsx
@@ -4,17 +4,21 @@ import { Link } from "react-router-dom"
 import img1 from "../../assets/realestatebeach.jpg"
 import img2 from "../../assets/realestatemountain.jpg"
 import img3 from "../../assets/realestatecountryside.jpg"
-
+import {request} from "../../util/fetchAPI"
+import { data } from 'jquery'
 function PopularProperties() {
-  const [numProperties,setNumProperties]=useState({})
+  const [numProperties,setNumProperties]=useState({
+  })
   useEffect(()=>{
     const fetchNumberProperties= async()=>{
       try{
-        const data=await request
+        const data=await request('/property/find/types',"GET")
+        setNumProperties(data)
       }catch(err){
         console.log(err.message)
       }
     }
+    fetchNumberProperties();
   },[])
   return (
     <div className={classes.container}>
@@ -24,19 +28,19 @@ function PopularProperties() {
           <h2>Best Types of Properties For You</h2>
         </div>
         <div className={classes.properties}>
-          <Link to={'/properties?type=beach&continent=1&priceRange=2'}>
+          <Link className={classes.property} to={'/properties?type=beach&continent=1&priceRange=2'}>
             <img src={img1}/>
-            <div className={classes.quantity}>34Properties</div>
+            <div className={classes.quantity}>{numProperties?.beach}Properties</div>
             <h5>Beach Properties</h5>
           </Link>
-          <Link to={'/properties?type=mountain&continent=1&priceRange=2'}>
+          <Link className={classes.property} to={'/properties?type=mountain&continent=1&priceRange=2'}>
             <img src={img2}/>
-            <div className={classes.quantity}>34Properties</div>
+            <div className={classes.quantity}>{numProperties?.mountain}Properties</div>
             <h5>Mountain Properties</h5>
           </Link>
-          <Link to={'/properties?type=village&continent=1&priceRange=2'}>
+          <Link className={classes.property} to={'/properties?type=village&continent=1&priceRange=2'}>
             <img src={img3}/>
-            <div className={classes.quantity}>34Properties</div>
+            <div className={classes.quantity}>{numProperties?.village}Properties</div>
             <h5>Village Properties</h5>
           </Link>
         </div>
--- client/src/util/fetchAPI.js
+++ client/src/util/fetchAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "http://localhost:4000"
 
 export const request = async (url, method, headers = {}, body = {}, isNotStringified = false) => {
     let res
